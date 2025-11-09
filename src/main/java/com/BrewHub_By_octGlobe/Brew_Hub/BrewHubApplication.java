05dc9fd05350c414932e382614ad53465446d913
nawo6TGJD <129091686+nawo6TGJD@users.noreply.github.com>
2025-11-09 15:23:05 +0530

    Initial commit

---
+++ src/main/java/com/BrewHub_By_octGlobe/Brew_Hub/BrewHubApplication.java
@@ -0,0 +1,13 @@
+package com.BrewHub_By_octGlobe.Brew_Hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrewHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrewHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BrewHub_By_octGlobe/Brew_Hub/BrewHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BrewHub_By_octGlobe.Brew_Hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrewHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
